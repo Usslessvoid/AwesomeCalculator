97be969557273eb5284b7f0da679799692011d60
Usslessvoid <36482404+Usslessvoid@users.noreply.github.com>
2018-06-20 18:46:13 +0300

    Finaly done multi-delete( press pencil icon to enter select mode; select items to delete; press button with rubbishbox and wait for a hour:))

--- AwesomeCalculator/app/src/main/java/com/example/drcreeper/awesomecalculator/fragments/HistoryListFragment.java
+++ AwesomeCalculator/app/src/main/java/com/example/drcreeper/awesomecalculator/fragments/HistoryListFragment.java
@@ -1,22 +1,31 @@
 package com.example.drcreeper.awesomecalculator.fragments;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.drcreeper.awesomecalculator.R;
 import com.example.drcreeper.awesomecalculator.asynktasks.AfterExecute;
+import com.example.drcreeper.awesomecalculator.asynktasks.Callback;
 import com.example.drcreeper.awesomecalculator.asynktasks.DeleteHistoryListAsyncTask;
+import com.example.drcreeper.awesomecalculator.asynktasks.DeleteItemsHistoryAsyncTask;
 import com.example.drcreeper.awesomecalculator.asynktasks.GetOperationsListAsyncTask;
 import com.example.drcreeper.awesomecalculator.historywriter.HistoryItemAdapter;
 
@@ -36,7 +45,8 @@ public class HistoryListFragment extends Fragment {
     TextView emptyText;
     List<String> list = new ArrayList<>();
     GetOperationsListAsyncTask reader = null;
-    ListAdapter adapter;
+    HistoryItemAdapter adapter;
+    boolean selectMode = false;
 
 
     @Nullable
@@ -51,7 +61,7 @@ public class HistoryListFragment extends Fragment {
         reader.setContext(view.getContext());
         reader.setCallback((list)->{
             this.list = list;
-            adapter = new HistoryItemAdapter(view.getContext(),list);
+            adapter = new HistoryItemAdapter(getContext(),list);
             listView.setAdapter(adapter);
         });
         reader.execute();
@@ -67,17 +77,49 @@ public class HistoryListFragment extends Fragment {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()){
             case R.id.choose_history:
-                listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
-                listView.setAdapter(adapter);
-                break;
-            case R.id.clear_all:
-                DeleteHistoryListAsyncTask deleter = new DeleteHistoryListAsyncTask(getActivity());
-                deleter.setOnCompleteListener(()->{
-                    list.clear();
-                    adapter = new HistoryItemAdapter(getContext(),list);
+                if(selectMode){
+                    adapter = new HistoryItemAdapter(getContext(), list);
                     listView.setAdapter(adapter);
-                });
-                deleter.execute();
+                    listView.setChoiceMode(ListView.CHOICE_MODE_NONE);
+                }else {
+                    adapter = new HistoryItemAdapter(getContext(), R.layout.history_item_selectable, list);
+                    listView.setAdapter(adapter);
+                    listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+
+                }
+
+
+                selectMode = !selectMode;
+                break;
+
+            case R.id.clear_all:
+                if(selectMode){
+
+                    DeleteItemsHistoryAsyncTask task = new DeleteItemsHistoryAsyncTask();
+                    task.setContext(listView.getContext());
+                    task.setInput(adapter.getPos());
+                    reader = new GetOperationsListAsyncTask();
+                    reader.setContext(listView.getContext());
+                    reader.setCallback((list)->{
+                        this.list = list;
+                        adapter = new HistoryItemAdapter(getContext(),list);
+                        listView.setAdapter(adapter);
+                    });
+                    task.setCallback(()->{
+
+                        reader.execute();
+                    });
+                    selectMode = !selectMode;
+                    task.execute();
+                }else {
+                    DeleteHistoryListAsyncTask deleter = new DeleteHistoryListAsyncTask(getActivity());
+                    deleter.setOnCompleteListener(() -> {
+                        list.clear();
+                        adapter = new HistoryItemAdapter(getContext(), list);
+                        listView.setAdapter(adapter);
+                    });
+                    deleter.execute();
+                }
                 break;
         }
         return true;
--- AwesomeCalculator/app/src/main/java/com/example/drcreeper/awesomecalculator/historywriter/HistoryItemAdapter.java
+++ AwesomeCalculator/app/src/main/java/com/example/drcreeper/awesomecalculator/historywriter/HistoryItemAdapter.java
@@ -7,17 +7,37 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.drcreeper.awesomecalculator.R;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class HistoryItemAdapter extends ArrayAdapter<String> {
-    public HistoryItemAdapter(Context context, List<String> list){
-        super(context, R.layout.history_item,list);
+
+    private List<Integer> pos = new ArrayList<>();
+
+    public List<Integer> getPos() {
+        return pos;
     }
 
+    public void setPos(List<Integer> pos) {
+        this.pos = pos;
+    }
+
+    private int tmpId;
+    public HistoryItemAdapter(Context context,List<String> list){
+        super(context, R.layout.history_item,list);
+        tmpId = R.layout.history_item;
+    }
+
+    public HistoryItemAdapter(Context context,int id,List<String> list){
+        super(context, R.layout.history_item,list);
+        tmpId = id;
+    }
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
@@ -25,13 +45,30 @@ public class HistoryItemAdapter extends ArrayAdapter<String> {
         ViewHolder viewHolder;
         if(convertView == null) {
             LayoutInflater inflater = LayoutInflater.from(getContext());
-            convertView = inflater.inflate(R.layout.history_item, parent, false);
+            convertView = inflater.inflate(tmpId, parent, false);
             viewHolder = new ViewHolder((TextView) convertView.findViewById(R.id.item_text));
             convertView.setTag(viewHolder);
         }else {
             viewHolder = (ViewHolder)convertView.getTag();
         }
         viewHolder.view.setText(getItem(position));
+        if(tmpId == R.layout.history_item_selectable){ //maybe not best solution
+            View finalConvertView = convertView;
+            convertView.setOnClickListener((d)->{
+                CheckBox c = (CheckBox)finalConvertView.findViewById(R.id.checkBox);
+                c.setChecked(!c.isChecked());
+                Integer i = new Integer(position);
+                if(pos.contains(i)){
+                    pos.remove(i);
+                }else {
+                    pos.add(i);
+                }
+
+            });
+        }
+/*
+        */
+        //convertView.findViewById(R.id.checkBox).setVisibility(View.VISIBLE);
         return convertView;
     }
 }
